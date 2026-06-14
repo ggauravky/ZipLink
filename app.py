d0fb56b2ba7331b206f82f50172b1fb706e93735
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-06-14 23:06:41 +0530

    work on database functions for CRUD

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import Flask
 
+from model import (
+    init_db,insert_url, 
+    get_url, 
+    increment_visit_count, 
+    get_all_urls, 
+    delete_url)
+
 app = Flask(__name__)
 
 @app.route('/')
--- model.py
+++ model.py
@@ -4,8 +4,7 @@ DB_NAME = 'database.db'
 
 def init_db():
     with sqlite3.connect(DB_NAME) as conn:
-        cursor = conn.cursor()
-        cursor.execute('''
+        conn.execute('''
             CREATE TABLE IF NOT EXISTS urls (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 original_url TEXT NOT NULL,
@@ -13,5 +12,35 @@ def init_db():
                 visit_count INTEGER DEFAULT 0
             )
         ''')
-        conn.commit()
         
+def insert_url(original_url, short_code):
+    with sqlite3.connect(DB_NAME) as conn:
+        conn.execute('''
+            INSERT INTO urls (original_url, short_code) VALUES (?, ?)
+        ''', (original_url, short_code))
+        
+def get_url(short_code):
+    with sqlite3.connect(DB_NAME) as conn:
+        cur=conn.execute('''
+            SELECT original_url, visit_count FROM urls WHERE short_code = ?
+        ''', (short_code,))
+        return cur.fetchone()
+        
+def increment_visit_count(short_code):
+    with sqlite3.connect(DB_NAME) as conn:
+        conn.execute('''
+            UPDATE urls SET visit_count = visit_count + 1 WHERE short_code = ?
+        ''', (short_code,))
+        
+def get_all_urls():
+    with sqlite3.connect(DB_NAME) as conn:
+        cur=conn.execute('''
+            SELECT original_url, short_code, visit_count FROM urls ORDER by visit_count DESC
+        ''')
+        return cur.fetchall()
+    
+def delete_url(short_code):
+    with sqlite3.connect(DB_NAME) as conn:
+        conn.execute('''
+            DELETE FROM urls WHERE short_code = ?
+        ''', (short_code,))
