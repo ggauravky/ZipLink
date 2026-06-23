b9430d78631a4e23bd3c8507e06d832474aa6401
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-06-23 17:23:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,6 +43,5 @@ def delete_short_url(short_code):
     delete_url_from_db(short_code)
     return redirect("/")
 
-if __name__ == '__main__':
-    app.run(debug=True)
-    
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1", port=8000)
